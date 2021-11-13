78cbbd7ca8eb0f28b8f0141b42e72af9ad05a6de
dror bahat <bahat.dror@gmail.com>
2021-11-13 13:21:48 +0200

    added local storage and X button

---
+++ index.js
@@ -0,0 +1,71 @@
+let textInputElement = document.getElementById("note-text-input")
+let dateInputElement = document.getElementById("date-selector")
+let timeInputElement = document.getElementById("time-selector")
+
+let notesOutputContainer = document.getElementById("output-main-container")
+
+const addNote = () => {
+    if (textInputElement.value === '' || dateInputElement.value === '' || timeInputElement.value === '') {
+        alert("All fields are necessary, please fill the note and try again.")
+        return
+    }
+
+    let newId = Math.floor(Math.random() * 10000)
+    let newNote = {
+        id: newId,
+        text: textInputElement.value,
+        date: dateInputElement.value,
+        time: timeInputElement.value
+    }
+
+    let jsonArr = localStorage.getItem("notes-list")
+    let notesList = JSON.parse(jsonArr)
+
+    if (notesList === null) {
+        notesList = []
+    }
+
+    notesList.push(newNote)
+
+    let toJson = JSON.stringify(notesList)
+    localStorage.setItem("notes-list", toJson)
+
+    loadNotes()
+
+    textInputElement.value = ''
+    dateInputElement.value = ''
+    timeInputElement.value = ''
+}
+
+const loadNotes = () => {
+    let jsonArr = localStorage.getItem("notes-list")
+    let notesList = JSON.parse(jsonArr)
+
+    if (notesList === null) {
+        return
+    }
+    let noteContainer = ``
+    for (let note of notesList) {
+        noteContainer += `<div id="output-note-container" class="note">
+                            <div id="note-text-output">${note.text}</div>
+                            <div class="time-and-date-output" id="time-and-date-output">${note.date}<br>${note.time}</div>
+                            <button type="button" id="delete-note" class="close" aria-label="Close" onclick="deleteNote(${note.id})">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                         </div>`
+    }
+    notesOutputContainer.innerHTML = noteContainer
+}
+
+const deleteNote = (id) => {
+    let jsonArr = localStorage.getItem("notes-list")
+    let notesList = JSON.parse(jsonArr)
+    
+    let findIdInNotesList = notesList.filter(el => el.id !== id)
+    notesList = findIdInNotesList
+    
+    let toJson = JSON.stringify(notesList)
+    localStorage.setItem("notes-list", toJson)
+    
+    loadNotes()
+}
