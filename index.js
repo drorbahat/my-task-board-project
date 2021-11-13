ea4c1884f481b14af00e06f7aab3474fd6d9b9ac
dror bahat <bahat.dror@gmail.com>
2021-11-13 14:03:48 +0200

    finished project!

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let timeInputElement = document.getElementById("time-selector")
 
 let notesOutputContainer = document.getElementById("output-main-container")
 
+
 const addNote = () => {
     if (textInputElement.value === '' || dateInputElement.value === '' || timeInputElement.value === '') {
         alert("All fields are necessary, please fill the note and try again.")
@@ -46,13 +47,13 @@ const loadNotes = () => {
     }
     let noteContainer = ``
     for (let note of notesList) {
-        noteContainer += `<div id="output-note-container" class="note">
-                            <div id="note-text-output">${note.text}</div>
-                            <div class="time-and-date-output" id="time-and-date-output">${note.date}<br>${note.time}</div>
-                            <button type="button" id="delete-note" class="close" aria-label="Close" onclick="deleteNote(${note.id})">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                         </div>`
+        noteContainer += `<div id="output-note-container" class="note" onmouseover="showClearButton()" onmouseout="hideClearButton()">
+        <div id="note-text-output" >${note.text}</div>
+        <div class="time-and-date-output" id="time-and-date-output">${note.date}<br>${note.time}</div>
+        <button type="button" id="delete-note" class="close" aria-label="Close" onclick="deleteNote(${note.id})" >
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>`
     }
     notesOutputContainer.innerHTML = noteContainer
 }
@@ -60,12 +61,20 @@ const loadNotes = () => {
 const deleteNote = (id) => {
     let jsonArr = localStorage.getItem("notes-list")
     let notesList = JSON.parse(jsonArr)
-    
+
     let findIdInNotesList = notesList.filter(el => el.id !== id)
     notesList = findIdInNotesList
-    
+
     let toJson = JSON.stringify(notesList)
     localStorage.setItem("notes-list", toJson)
-    
+
     loadNotes()
+}
+
+const showClearButton = () => {
+    document.querySelector("#delete-note").style.visibility = "visible";
+}
+
+const hideClearButton = () => {
+    document.querySelector("#delete-note").style.visibility = "hidden";
 }
